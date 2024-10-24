e618ed52306258abc7575e651a754649c77c0625
Samandar <samandarnormuminov2021@gmail.com>
2024-10-24 10:05:56 +0500

    Initial Commit

---
+++ Simple CalculatorTests/Simple_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_CalculatorTests.swift
+//  Simple CalculatorTests
+//
+//  Created by Samandar on 24/10/24.
+//
+
+import Testing
+@testable import Simple_Calculator
+
+struct Simple_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
